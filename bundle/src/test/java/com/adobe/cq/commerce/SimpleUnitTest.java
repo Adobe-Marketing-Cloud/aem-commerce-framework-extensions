cc621b6f3e8d9c74bee161db12228c33c8f1258e
Varun V <vvenkata@adobe.com>
2015-01-20 20:35:06 +1100

    Initial COmmit with project structure

---
+++ bundle/src/test/java/com/adobe/cq/commerce/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package com.adobe.cq.commerce;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleUnitTest {
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+
+}
