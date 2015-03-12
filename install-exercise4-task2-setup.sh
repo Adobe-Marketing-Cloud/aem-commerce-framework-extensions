bced348ea3f07dfcf34daced21990c865301955c
Varun V <vvenkata@adobe.com>
2015-03-12 15:37:02 +1100

    automated scripts

---
+++ install-exercise4-task2-setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ 
+# Bash script to install solutions
+ 
+# Author : Varun Venkataraman (Adobe Global Services)
+# Version : 0.1
+# Last updated : March 01, 2015
+ 
+ 
+# Host that CQ runs on
+git add *
+git stash
+git pull
+sh cleanup.sh
+git checkout exercise4-task2
+sh prep_dependencies.sh
+mvn eclipse:clean eclipse:eclipse
